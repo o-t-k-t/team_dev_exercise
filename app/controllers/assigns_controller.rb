ed44a3bcbd141c8163e1e10a3e105708a716991b
helium <wetcondition@gmail.com>
2018-12-04 16:00:53 +0900

    チームメンバー削除可否判定のバグ修正

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -16,7 +16,7 @@ class AssignsController < ApplicationController
     assign = Assign.find(params[:id])
     assigned_user = assign.user
 
-    if !(assign.team.owner == current_user && assigned_user == current_user)
+    if !(assign.team.owner == current_user || assigned_user == current_user)
       redirect_to team_url(params[:team_id]), notice: 'リーダー・本人以外はメンバー削除はできません。'
     elsif assigned_user == assign.team.owner
       redirect_to team_url(params[:team_id]), notice: 'リーダーは削除できません。'
