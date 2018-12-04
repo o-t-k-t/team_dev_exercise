e463dd97c563b8bd4e0eaa3d811e5e74c905565c
helium <wetcondition@gmail.com>
2018-12-04 15:58:29 +0900

    View実装

--- app/helpers/team_helper.rb
+++ app/helpers/team_helper.rb
@@ -2,4 +2,12 @@ module TeamHelper
   def default_img(image)
     image.presence || 'default.jpg'
   end
+
+  def deletable_asign?(assign)
+    assign.team.owner == current_user || assign.user == current_user
+  end
+
+  def editable_team?(team)
+    team.owner == current_user
+  end
 end
