265e9238496b48b40a49da7acba90795f8b73df0
helium <wetcondition@gmail.com>
2018-12-05 17:35:12 +0900

    コントーラのチームリーダー変更制限を実装

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -1,10 +1,15 @@
 class OwnershipsController < ApplicationController
+  include TeamHelper
+
   def update
     team = Team.friendly.find(params[:team_id])
     new_owner = User.find(params[:user_id])
 
-    team.change_leader(new_owner)
-
-    redirect_to team, notice: 'リーダー権限を移しました'
+    if can_delegate_ownership?(current_user, team)
+      team.change_leader(new_owner)
+      redirect_to team, notice: 'リーダー権限を移しました'
+    else
+      redirect_to team, notice: '現在のリーダー以外、リーダーを変更できません'
+    end
   end
 end
--- app/helpers/team_helper.rb
+++ app/helpers/team_helper.rb
@@ -2,4 +2,8 @@ module TeamHelper
   def default_img(image)
     image.presence || 'default.jpg'
   end
+
+  def can_delegate_ownership?(user, team)
+    user == team.owner
+  end
 end
